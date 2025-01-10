df786c87bb7b252b4b0ea3117be622c062bdb10e
Rizky <rizky@avolut.com>
2025-01-11 05:34:47 +0700

    checkpoint

--- backend/server.ts
+++ backend/server.ts
@@ -1,5 +1,15 @@
+import system from "standalone/system";
+
 export const server: PrasiServer = {
+  async init(arg) {
+    system.init(arg.port);
+  },
   http({ handle, req }) {
+    const response = system.router.handle(req);
+    if (response) {
+      return response;
+    }
+
     return handle(req);
   },
 };
